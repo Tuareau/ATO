db506378f6b4345207a24d7eb505c2a428b51418
Pavel Matviyuk <65852860+Tuareau@users.noreply.github.com>
2021-02-25 01:35:38 +0300

    Unnecessary comments deleted

--- ato.c
+++ ato.c
@@ -1,7 +1,5 @@
 ﻿#include "header.h"
 
-// размер массива указателей на самолеты
-// и количество указателей в нем
 int size, p_amount;
 
 int main(void)
@@ -9,10 +7,8 @@ int main(void)
 	graph = NULL;
 	size = p_amount = 0;
 	char fname[] = "ato.dat";
-	// чтение файла и начало работы
 	start(fname);
 	print_graph();
-	// запись в файл и освобождение памяти
 	end(fname);
 	return 0;
 }
